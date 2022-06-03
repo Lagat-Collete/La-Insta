1a36234986b5e34677029b0f14340083d35399c8
colletemine <jepngetichcolleta@gmail.com>
2022-06-03 17:02:52 +0300

    Add image model

--- Insta/settings.py
+++ Insta/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'insta',
             'USER': 'lagat',
-            'PASSWORD': '12345',
+            'PASSWORD': 'Access',
             'HOST': 'localhost',
             'PORT': '',
     }
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- post/models.py
+++ post/models.py
@@ -1,3 +1,31 @@
+from xml.etree.ElementTree import Comment
 from django.db import models
+from django.contrib.auth.models import User
+from distutils.command.upload import upload
+import datetime as dt
+from django.utils.text import slugify
+from cloudinary.models import CloudinaryField 
+
 
 # Create your models here.
+
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length=200)
+    profile = models.ForeignKey(User)
+    Comments = models.TextField()  
+    likes = models.ManyToManyField(User,)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_
+
+
+ 
+
